51a1c86daf5246e5281882d09e057aa414638e90
saptarshi chakraborty <saptarshi34.sc@gmail.com>
2022-06-08 17:33:30 +0530

    callback-opertaion-demo

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,13 +1,27 @@
  console.log('before')
- const getData = getUserId(1)
- console.log(getData)
+ getUserId(1,(user)=>{
+   console.log('user',user)
+  getRepo(user.gitUserid,(repos)=>{
+    console.log('repos',repos)
+  })
+ })
+ //console.log(getData)
+ //callbacks
+ //async
+ //promises
 
- function getUserId(id){
+ function getUserId(id,callback){
  setTimeout(()=>{
-   console.log('wait for this app  running')
-   return {id:id,gitUserid:'saptarshi'}
+   console.log('wait for fetch data from Database')
+   callback({id:id,gitUserid:'saptarshi'})
  },2000)
- return {id:id,gitUserid:'saptarshi'}
+ //return {id:id,gitUserid:'saptarshi'}
 }
+console.log('after')
 
- console.log('after')
+function getRepo(username,callback){
+  setTimeout(()=>{
+    console.log('fetching Github repo..')
+    callback(['repo1','repo2','repo3'])
+  },2000)
+}
