485f2c9196f0d61c5a24eea99edcc08b41a37a1d
saptarshi chakraborty <saptarshi34.sc@gmail.com>
2022-06-09 16:07:13 +0530

    callback hell

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,10 +1,19 @@
  console.log('before')
- getUserId(1,(user)=>{
-   console.log('user',user)
-  getRepo(user.gitUserid,(repos)=>{
-    console.log('repos',repos)
-  })
- })
+
+ //Async function::
+
+ getUserId(1,userData)
+
+ function userData(user){
+  getRepo(user.gitUserid,getRepos)
+ }
+ function getRepos(repos){
+  getCommit(repos[0],displayCommit)
+ }
+ function displayCommit(commit){
+   console.log(commit)
+ }
+ 
  //console.log(getData)
  //callbacks
  //async
@@ -23,5 +32,11 @@ function getRepo(username,callback){
   setTimeout(()=>{
     console.log('fetching Github repo..')
     callback(['repo1','repo2','repo3'])
+  },2000)
+}
+function getCommit(repo,callback){
+  setTimeout(()=>{
+    console.log('getting commit message')
+    callback({repo:repo,commit:'new-commit'})
   },2000)
 }
