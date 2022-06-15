dd5f554a43f8493bf198a6c65f40097df57b4f15
saptarshi1992@34 <saptarshi34.sc@gmail.com>
2022-06-16 01:47:45 +0530

    import json into mongod

--- mongodb-demo/mongo-exercise.js
+++ mongodb-demo/mongo-exercise.js
@@ -1,29 +1,27 @@
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://0.0.0.0:27017/mongo-exercises')
-.then(()=>console.log('Database is connected'))
-.catch((err)=>console.error('Database is not connected;',err))
+    .then(() => console.log('Database is connected'))
+    .catch((err) => console.error('Database is not connected;', err))
 
 const createSchema = mongoose.Schema({
-    tags:[String],
-    date:Date,
-    name:String,
-    author:String,
-    isPublished:Boolean,
-    price:Number
+    tags: [String],
+    date: Date,
+    name: String,
+    author: String,
+    isPublished: Boolean,
+    price: Number
 })
 
-const Course = new mongoose.model('Course',createSchema)
+const Course = new mongoose.model('Course', createSchema)
 
 /*const course = new Course({
 
 })*/
-async function getdata(){
+async function getdata() {
     const courses = await Course
-                        .find({isPublished:true})
-                        .sort({name:-1})
-                        .select({name:1,author:1})
-
-        
+        .find({ isPublished: true, tags: 'backend' })
+        .sort({ name: -1 })
+        .select({ name: 1, author: 1 })
     console.log(courses)
 }
 getdata()
